2a5634aa84e02a8244d900fd9ad64c9b7d43417a
Wolfgang Pfretzschner <wolfgangp@live.com>
2018-03-29 12:16:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ function theBeatlesPlay(musicians,instruments){
 
 theBeatlesPlay(['hans', 'berry'],['flut','gitar'])
 
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
 function johnLennonFacts(facts){
-  
+  while(facts.length>0){
+    
+  }
 }
